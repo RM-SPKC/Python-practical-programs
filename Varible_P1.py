db4efc9d9e152b6d45d36fb15ed175566ed47391
RM <rm.spkcit@gmail.com>
2025-02-28 14:38:08 +0530

    Add files via upload

---
+++ Varible_P1.py
@@ -0,0 +1,7 @@
+print("\n\t\t\tArea of the circle")
+print("\t\t\t-----------------")
+pi=3.14
+r=int(input("\nEnter the radius :"))
+area=pi*r*r
+circumference=2*pi*r
+print("\n\n\tArea of the given circle: ",area)
