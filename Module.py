f99d71bbacd263c42e0eb03927013428e7df4710
RM <rm.spkcit@gmail.com>
2025-03-05 06:55:31 +0530

    Add files via upload

---
+++ Module.py
@@ -0,0 +1,8 @@
+print("\n\t\t\tModule")
+print("\t\t\t------")
+import power as p
+x=int(input("Enter a number: "))
+y=int(input("Enter the power value:"))
+print("\n\nSquare of",x,"is: ",p.square(x))
+print("Cube of",x,"is: ",p.cube(x))
+print(x ,"to the power",y,"is: ",p.pow(x,y))
